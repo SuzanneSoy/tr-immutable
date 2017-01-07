513c94b991782d6ed2ddfb0d6c1fb9d33fc589e4
Georges Dupéron <georges.duperon@gmail.com>
2017-01-08 00:04:00 +0100

    Failed attempt at disguising a vector as struct using make-struct-info, struct/c (which TR uses internally in make-predicate) seems to ignore the predicate supplied to make-struct-info.

--- main.rkt
+++ main.rkt
@@ -3,11 +3,18 @@
 (require typed/racket/unsafe)
 
 (provide IVectorof
-         (rename-out [new-ivector ivector]))
+         IVectorof2
+         (rename-out [new-ivector ivector])
+         (rename-out [new-ivector2 ivector2])
+         ivector2-v)
 
 (unsafe-require/typed tr-immutable/private/unsafe
                       [#:struct (A) ivector ([v : (Listof A)])
-                       #:type-name IVectorof])
+                       #:type-name IVectorof]
+                      [#:struct (A) ivector2 ([v : (Listof A)])
+                       #:constructor-name make-ivector2
+                       #:type-name IVectorof2]
+                      [new-ivector2 (∀ (A) (→ A * (IVectorof2 A)))])
 
 (: new-ivector (∀ (A) (→ A * (IVectorof A))))
 (define (new-ivector . vs)
